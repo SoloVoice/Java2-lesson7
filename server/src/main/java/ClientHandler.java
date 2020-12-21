16f60f51fc875ae069e1884499efc622a94eda7c
SoloVoice <a.v.rustamov@gmail.com>
2020-12-21 18:22:46 +0300

    clear

---
+++ server/src/main/java/ClientHandler.java
@@ -0,0 +1,88 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String nickname;
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        // /auth login1 password1
+                        String[] subStrings = str.split(" ");
+                        if (subStrings.length >= 3 && subStrings[0].equals("/auth")) {
+                            String nickFromDB = SQLHandler.getNickByLoginAndPassword(subStrings[1], subStrings[2]);
+                            if (nickFromDB != null) {
+                                sendMsg("/authok");
+                                server.subscribe(this);
+                                nickname = nickFromDB;
+                                break;
+                            }
+                        }
+                    }
+
+                    while (true) {
+                        String str = in.readUTF();
+                        System.out.println("Сообщение от клиента: " + str);
+                        if (str.indexOf("/end") == 0) {
+                            break;
+//                        } else if (str.indexOf("/w") == 0) {
+//                            String[] msgDivide = str.split(" ");
+//                            String[] clearPrivateMsg = new String[msgDivide.length-2];
+//                            System.arraycopy(msgDivide, 2, clearPrivateMsg, 0, msgDivide.length-2);
+//                            String privateMsg = String.join(" ", clearPrivateMsg);
+//                            server.privatMsg(nickname, msgDivide[1], privateMsg);
+                        } else {
+                            server.broadcastMsg(nickname + ": " + str);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    server.unsubscribe(this);
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
---
+++ server/src/main/java/SQLHandler.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+
+public class SQLHandler {
+    private static Connection connection;
+    private static Statement statement;
+
+    public static void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:server/src/main/resources/usersdb.db");
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getNickByLoginAndPassword(String login, String password) {
+        try {
+            ResultSet rs = statement.executeQuery("SELECT nick FROM users WHERE login ='" + login + "' AND pass = '" + password + "'");
+            if (rs.next()) {
+                return rs.getString("nick");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
